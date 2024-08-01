0ffb944f905a576ff02608ed228f87d66481c83a
Vlad Stan <stan.v.vlad@gmail.com>
2024-08-01 15:40:28 +0300

    feat: add empty `Lightning Address` tab

--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -1,5 +1,5 @@
 <template v-if="loaded">
-  <q-page class="q-pa-sm container">
+  <q-page class="container">
     <div class="q-gutter-md">
       <q-breadcrumbs class="text-grey-4 q-mb-lg" active-color="secondary">
         <q-breadcrumbs-el icon="home" to="/" />
@@ -7,193 +7,220 @@
         <q-breadcrumbs-el :label="user_details.name" />
       </q-breadcrumbs>
     </div>
-    <q-card class="nostr-card" bordered v-if="!user_details.name">
-      <q-item>
-        <q-item-section avatar>
-          <q-skeleton type="QAvatar" />
-        </q-item-section>
+    <q-card class="nostr-card text-white no-shadow q-mb-xl" bordered>
+      <q-tabs
+        v-model="tab"
+        dense
+        class="text-grey"
+        active-color="secondary"
+        indicator-color="secondary"
+        align="justify"
+        narrow-indicator
+      >
+        <q-tab name="identifier" label="NIP05 Identifier" />
+        <q-tab name="lnaddress" label="Lightning Address" />
+      </q-tabs>
 
-        <q-item-section>
-          <q-item-label>
-            <q-skeleton type="text" />
-          </q-item-label>
-          <q-item-label caption>
-            <q-skeleton type="text" />
-          </q-item-label>
-        </q-item-section>
-      </q-item>
+      <q-separator />
 
-      <q-skeleton height="200px" square />
-    </q-card>
-    <q-card v-else class="nostr-card text-white no-shadow q-mb-xl" bordered>
-      <q-card-section>
-        <q-list dark class="row">
-          <q-item class="col-12">
-            <q-item-section>
+      <q-tab-panels class="nostr-card" v-model="tab" animated>
+        <q-tab-panel name="identifier" class="q-pa-none">
+          <div v-if="!user_details.name">
+            <q-item>
+              <q-item-section avatar>
+                <q-skeleton type="QAvatar" />
+              </q-item-section>
+
+              <q-item-section>
+                <q-item-label>
+                  <q-skeleton type="text" />
+                </q-item-label>
+                <q-item-label caption>
+                  <q-skeleton type="text" />
+                </q-item-label>
+              </q-item-section>
+            </q-item>
+
+            <q-skeleton height="200px" square />
+          </div>
+          <div v-else>
+            <q-card-section>
+              <q-list dark class="row">
+                <q-item class="col-12">
+                  <q-item-section>
+                    <div>
+                      <div class="text-h6">
+                        Identifier
+                        <q-badge
+                          class="nostr-card"
+                          removable
+                          color="primary"
+                          text-color="white"
+                          ><span v-if="user_details.active">Active</span>
+                          <span v-else>Inactive</span>
+                        </q-badge>
+                      </div>
+
+                      <div v-if="user_details.expiresAt" class="text-caption">
+                        Expires at
+                        <span
+                          v-text="
+                            new Date(
+                              user_details.expiresAt * 1000
+                            ).toLocaleDateString()
+                          "
+                        ></span>
+                      </div>
+                    </div>
+                  </q-item-section>
+                  <q-item-section side>
+                    <q-btn
+                      type="a"
+                      :href="`https://metadata.nostr.com?pubkey=${user_details.pubkey}`"
+                      target="_blank"
+                      label="Edit Nostr Profile"
+                      class="text-capitalize"
+                      rounded
+                      color="secondary"
+                      text-color="primary"
+                    ></q-btn>
+                  </q-item-section>
+                </q-item>
+              </q-list>
+            </q-card-section>
+            <q-card-section class="q-pa-sms">
+              <q-list dark class="row">
+                <q-item class="col-12">
+                  <q-item-section side>
+                    <q-avatar size="100px">
+                      <q-img
+                        v-if="user_details.picture"
+                        :src="user_details.picture"
+                        spinner-color="secondary"
+                        spinner-size="52px"
+                        :ratio="1"
+                      />
+                      <NostrHeadIcon v-else color="secondary" />
+                    </q-avatar>
+                  </q-item-section>
+                  <q-item-section>
+                    <q-item-label>{{ user_details.name }}</q-item-label>
+                    <q-item-label caption class="ellipsis"
+                      >{{ user_details.pubkey }}
+                    </q-item-label>
+                    <q-item-label>
+                      <q-btn
+                        :to="`/pubkey/${user_details.pubkey}`"
+                        rounded
+                        class="text-capitalize q-mt-sm"
+                        color="secondary"
+                        text-color="primary"
+                        label="View all"
+                        size="sm"
+                        ><q-tooltip
+                          >View all NIP05 identities associated with this public
+                          key</q-tooltip
+                        ></q-btn
+                      >
+                    </q-item-label>
+                  </q-item-section>
+                </q-item>
+
+                <q-item class="col-12">
+                  <q-item-section>
+                    <q-input
+                      class=""
+                      dark
+                      standout
+                      v-model="user_details.pubkey"
+                      label="Public Key"
+                      hint="Public key associated with this identifier. Hex or Npub format."
+                    />
+                  </q-item-section>
+                </q-item>
+                <q-item class="col-12 q-mt-lg">
+                  <q-item-section>
+                    <q-input
+                      dark
+                      standout
+                      v-model="addRelayValue"
+                      @keydown.enter="addRelayFn(addRelayValue)"
+                      type="url"
+                      label="wss://relay....."
+                      hint="Add a relay"
+                    >
+                      <q-btn
+                        @click="addRelayFn(addRelayValue)"
+                        dense
+                        flat
+                        icon="add"
+                      ></q-btn>
+                      <template v-slot:after> </template>
+                    </q-input>
+                  </q-item-section>
+                </q-item>
+              </q-list>
+            </q-card-section>
+            <q-card-section class="q-ml-sm">
               <div>
-                <div class="text-h6">
-                  Identifier
-                  <q-badge
-                    class="nostr-card"
-                    removable
-                    color="primary"
-                    text-color="white"
-                    ><span v-if="user_details.active">Active</span>
-                    <span v-else>Inactive</span>
-                  </q-badge>
-                </div>
-
-                <div v-if="user_details.expiresAt" class="text-caption">
-                  Expires at
-                  <span
-                    v-text="
-                      new Date(
-                        user_details.expiresAt * 1000
-                      ).toLocaleDateString()
-                    "
-                  ></span>
-                </div>
+                <q-btn
+                  @click="refreshRelaysFromNostr"
+                  rounded
+                  color="secondary"
+                  text-color="primary"
+                  size="sm"
+                  >Add Relays from Nostr Profile</q-btn
+                >
               </div>
-            </q-item-section>
-            <q-item-section side>
+            </q-card-section>
+            <q-card-section>
+              <div class="q-mt-md">
+                <q-chip
+                  v-for="relay in user_details.relays"
+                  :key="relay"
+                  class="nostr-card"
+                  removable
+                  @remove="removeRelayFn(relay)"
+                  color="primary"
+                  text-color="white"
+                >
+                  <span v-text="relay"></span>
+                </q-chip>
+              </div>
+            </q-card-section>
+
+            <q-separator color="secondary"></q-separator>
+
+            <q-card-actions align="right" class="q-pa-md">
               <q-btn
-                type="a"
-                :href="`https://metadata.nostr.com?pubkey=${user_details.pubkey}`"
-                target="_blank"
-                label="Edit Nostr Profile"
+                disabled
+                label="Renew"
                 class="text-capitalize"
                 rounded
                 color="secondary"
                 text-color="primary"
-              ></q-btn>
-            </q-item-section>
-          </q-item>
-        </q-list>
-      </q-card-section>
-      <q-card-section class="q-pa-sm">
-        <q-list dark class="row">
-          <q-item class="col-12">
-            <q-item-section side>
-              <q-avatar size="100px">
-                <q-img
-                  v-if="user_details.picture"
-                  :src="user_details.picture"
-                  spinner-color="secondary"
-                  spinner-size="52px"
-                  :ratio="1"
-                />
-                <NostrHeadIcon v-else color="secondary" />
-              </q-avatar>
-            </q-item-section>
-            <q-item-section>
-              <q-item-label>{{ user_details.name }}</q-item-label>
-              <q-item-label caption class="ellipsis"
-                >{{ user_details.pubkey }}
-              </q-item-label>
-              <q-item-label>
-                <q-btn
-                  :to="`/pubkey/${user_details.pubkey}`"
-                  rounded
-                  class="text-capitalize q-mt-sm"
-                  color="secondary"
-                  text-color="primary"
-                  label="View all"
-                  size="sm"
-                  ><q-tooltip
-                    >View all NIP05 identities associated with this public
-                    key</q-tooltip
-                  ></q-btn
-                >
-              </q-item-label>
-            </q-item-section>
-          </q-item>
-
-          <q-item class="col-12">
-            <q-item-section>
-              <q-input
-                class=""
-                dark
-                standout
-                v-model="user_details.pubkey"
-                label="Public Key"
-                hint="Public key associated with this identifier. Hex or Npub format."
-              />
-            </q-item-section>
-          </q-item>
-          <q-item class="col-12 q-mt-lg">
-            <q-item-section>
-              <q-input
-                dark
-                standout
-                v-model="addRelayValue"
-                @keydown.enter="addRelayFn(addRelayValue)"
-                type="url"
-                label="wss://relay....."
-                hint="Add a relay"
               >
-                <q-btn
-                  @click="addRelayFn(addRelayValue)"
-                  dense
-                  flat
-                  icon="add"
-                ></q-btn>
-                <template v-slot:after> </template>
-              </q-input>
-            </q-item-section>
-          </q-item>
-        </q-list>
-      </q-card-section>
-      <q-card-section class="q-ml-sm">
-        <div>
-          <q-btn
-            @click="refreshRelaysFromNostr"
-            rounded
-            color="secondary"
-            text-color="primary"
-            size="sm"
-            >Add Relays from Nostr Profile</q-btn
-          >
-        </div>
-      </q-card-section>
-      <q-card-section>
-        <div class="q-mt-md">
-          <q-chip
-            v-for="relay in user_details.relays"
-            :key="relay"
-            class="nostr-card"
-            removable
-            @remove="removeRelayFn(relay)"
-            color="primary"
-            text-color="white"
-          >
-            <span v-text="relay"></span>
-          </q-chip>
-        </div>
-      </q-card-section>
+                <!-- todo: make this true -->
+                <q-tooltip
+                  >Can renew six months before expiration date.</q-tooltip
+                >
+              </q-btn>
+              <q-btn
+                @click="updateUserIdentifier"
+                rounded
+                class="text-capitalize"
+                color="secondary"
+                text-color="primary"
+                label="Update NIP05"
+              />
+            </q-card-actions>
+          </div>
+        </q-tab-panel>
 
-      <q-separator color="secondary"></q-separator>
-
-      <q-card-actions align="right">
-        <q-btn
-          disabled
-          label="Renew"
-          class="text-capitalize"
-          rounded
-          color="secondary"
-          text-color="primary"
-        >
-          <!-- todo: make this true -->
-          <q-tooltip>Can renew six months before expiration date.</q-tooltip>
-        </q-btn>
-        <q-btn
-          @click="updateUserIdentifier"
-          rounded
-          class="text-capitalize"
-          color="secondary"
-          text-color="primary"
-          label="Update NIP05"
-        />
-      </q-card-actions>
+        <q-tab-panel name="lnaddress">
+          <div class="text-h6">Lightning Address</div>
+        </q-tab-panel>
+      </q-tab-panels>
     </q-card>
   </q-page>
 </template>
@@ -214,6 +241,7 @@ const $nostr = useNostrStore();
 
 const props = defineProps(["name"]);
 
+const tab = ref("identifier");
 const user_details = ref({});
 const addRelayValue = ref("");
 
